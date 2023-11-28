fed902024983a1b793a71cf93e2189f8e6a19290
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-11-27 21:11:08 -0500

    build(materialUI): Add dependencies and import fontsource

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//Desc: Entry point of the app
+// ============================================================
 
+// Importing libraries and styles
+// ============================================================
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+// ============================================================
+// Importing fontsource
+// ============================================================
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+// ============================================================
+
+// Rendering the app
+// ============================================================
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
+// ============================================================
