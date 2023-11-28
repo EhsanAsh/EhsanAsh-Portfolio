0575d6219d126982f9186c5db467cc5d76cacb9a
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-11-28 18:12:04 -0500

    feat(main.jsx): Refactor import statements and add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,33 @@
 //Desc: Entry point of the app
+// used the following repo as reference for the CssBaseline:
+// (https://github.com/mui/material-ui/tree/master/examples/material-ui-vite)
 // ============================================================
 
-// Importing libraries and styles
+// Importing libraries
 // ============================================================
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+// CssBaseline kickstart an elegant, consistent, and simple baseline to build upon.
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// ============================================================
+
+// Importing pages and components
+// ============================================================
 import App from './App.jsx';
-import './index.css';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+import Error from './pages/Error.jsx';
 // ============================================================
+
+// Importing custom Styles and Utils
+// ============================================================
+import './index.css';
+import theme from './utils/theme.js';
+// ============================================================
+
 // Importing fontsource
 // ============================================================
 import '@fontsource/roboto/300.css';
@@ -15,9 +36,53 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 // ============================================================
 
+// Creating routes
+// ============================================================
+const routes = [
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error message='Sorry, an unexpected error has occurred.' />,
+        children: [
+            {
+                index: true,
+                element: <About />,
+            },
+            {
+                path: '/portfolio',
+                element: <Portfolio />,
+            },
+            {
+                path: '/resume',
+                element: <Resume />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            },
+            {
+                path: '*',
+                element: <Error message='Sorry, the page you requested could not be found.' />,
+            },
+        ],
+    }
+];
+
+const router = createBrowserRouter(routes);
+// ============================================================
+
+// Selecting the root element
+// ============================================================
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+// ============================================================
+
 // Rendering the app
 // ============================================================
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />,
+root.render(
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+    </ThemeProvider>,
 );
 // ============================================================
