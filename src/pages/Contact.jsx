fec34406b9b3725fc2dc3e2e2bde379ebed6e667
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-11-30 22:25:41 -0500

    chore(main): Add Contact page and update routes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
 // Desc: This file contains the code for the contact page using form component.
 // ========================================================
+// TODO: Add Style
+// TODO: Add other features and components
+// TODO: Add style to Form component
 
 // importing form component
 // ========================================================
