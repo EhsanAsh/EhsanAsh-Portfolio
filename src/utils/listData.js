85ad3f20b7fbfedcaeddf9b28b16ccf48df47a05
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-12-01 00:58:33 -0500

    feat(listData): Add listData.js file with front-end and back-end proficiency data

---
+++ src/utils/listData.js
@@ -0,0 +1,117 @@
+// Desc: This file exports title and item data for the list component. There are two titles: Front - end Proficiency and Back - end Proficiency.
+// =================================================================
+
+// Defining the data
+// =================================================================
+const frontEnd = {
+    title: 'Front-end Proficiencies',
+    items: [
+        {
+            name: 'HTML',
+            icon: 'fab fa-html5',
+            color: '#e34f26',
+        },
+        {
+            name: 'CSS',
+            icon: 'fab fa-css3-alt',
+            color: '#264de4',
+        },
+        {
+            name: 'JavaScript',
+            icon: 'fab fa-js-square',
+            color: '#f7df1e',
+        },
+        {
+            name: 'jQuery',
+            icon: 'fas fa-code',
+            color: '#0769ad',
+        },
+        {
+            name: 'Bootstrap',
+            icon: 'fab fa-bootstrap',
+            color: '#563d7c',
+        },
+        {
+            name: 'Material UI',
+            icon: 'fab fa-react',
+            color: '#0081CB',
+        },
+        {
+            name: 'Responsive Design',
+            icon: 'fas fa-mobile-alt',
+            color: '#000000',
+        },
+        {
+            name: 'PWA',
+            icon: 'fas fa-mobile-alt',
+            color: '#000000',
+        },
+        {
+            name: 'React',
+            icon: 'fab fa-react',
+            color: '#61DBFB',
+        },
+        {
+            name: 'Redux',
+            icon: 'fab fa-react',
+            color: '#764abc',
+        },
+    ],
+};
+
+const backEnd = {
+    title: 'Back-end Proficiencies',
+    items: [
+        {
+            name: 'Node.js',
+            icon: 'fab fa-node-js',
+            color: '#68a063',
+        },
+        {
+            name: 'Express',
+            icon: 'fas fa-server',
+            color: '#000000',
+        },
+        {
+            name: 'MongoDB',
+            icon: 'fas fa-database',
+            color: '#47a248',
+        },
+        {
+            name: 'MySQL and Sequelize',
+            icon: 'fas fa-database',
+            color: '#4479a1',
+        },
+        {
+            name: 'RESTful APIs',
+            icon: 'fas fa-server',
+            color: '#000000',
+        },
+        {
+            name: 'GraphQL',
+            icon: 'fas fa-server',
+            color: '#e535ab',
+        },
+        {
+            name: 'Apollo',
+            icon: 'fas fa-server',
+            color: '#000000',
+        },
+        {
+            name: 'JWT',
+            icon: 'fas fa-server',
+            color: '#000000',
+        },
+        {
+            name: 'MERN Stack',
+            icon: 'fas fa-server',
+            color: '#000000',
+        },
+    ],
+};
+// =================================================================
+
+// Exporting the data
+// =================================================================
+export { frontEnd, backEnd };
+// =================================================================
