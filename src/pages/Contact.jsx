c765cc38c6ab3743d68b95707774865fbc962f43
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-11-30 22:21:25 -0500

    feat(contact): Add contact page with form component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,29 @@
+// Desc: This file contains the code for the contact page using form component.
+// ========================================================
+
+// importing form component
+// ========================================================
+import Form from '../components/form/Form';
+// ========================================================
+
+// Creating the contact page
+// ========================================================
+const Contact = () => {
+
+    return (
+
+        <div>
+
+            <Form />
+
+        </div>
+
+    );
+
+};
+// ========================================================
+
+// Exporting the contact page
+// ========================================================
+export default Contact;
+// ========================================================
