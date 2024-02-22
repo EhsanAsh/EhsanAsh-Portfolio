ba810dbb96224d624b8adf9501be72846ca280e6
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-02-21 23:56:16 -0500

    perf: Add foundational style overrides for colors and fonts

---
+++ src/theme/foundations/colors.js
@@ -0,0 +1,23 @@
+// Desc: This file contains the foundational style overrides for colors
+// ============================================================
+
+// Define Overrides
+// ============================================================
+const colors = {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#fff',
+    brand: {
+        orange: '#fca311',
+        lightGreen: '#9AA61A',
+        darkGreen: '#006571',
+        lightGray: '#e5e5e5',
+        error: '#9a031e',
+    }
+};
+// ============================================================
+
+// Export Overrides
+// ============================================================
+export default colors;
+// ============================================================
