03e15561375e8e7ee54366eee8d83cdc99f3dcc9
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-02-25 02:26:30 -0500

    feat: Add image and update header layout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,12 +5,13 @@
 
 // Importing modules
 // ============================================================
-import { Flex, Spacer, Box, Button, Container } from '@chakra-ui/react';
+import { Flex, Spacer, Box, Button, Container, Text, Image } from '@chakra-ui/react';
 // ============================================================
 
 // Importing images
 // ============================================================
 import ehsan from '../../assets/image/ehsan.png';
+import Hero from '../../assets/image/Hero.jpg';
 // ============================================================
 
 // Creating the component
@@ -20,40 +21,84 @@ const Header = () => {
 
     return (
 
-        <Flex minWidth='max-content' alignItems='center' gap='2'>
+        <Flex
+            direction={'column'}
+            width='100%'
+            minWidth='max-content'
+            alignItems='center'
+            justifyContent='center'
+            flexShrink={0}
+            px='10'
+            py='20'
+            gap='10'
+            mb='10'
+            bgImage={Hero}
+            bgPosition={'center'}
+        >
             <Box>
                 <Button>
                     <a href="/">
-                                Home
+                        Home
                     </a>
                 </Button>
                 <Button>
                     <a href="/portfolio">
-                                Portfolio
+                        Portfolio
                     </a>
                 </Button>
                 <Button>
                     <a href="/projects">
-                                Projects
+                        Projects
                     </a>
                 </Button>
                 <Button>
                     <a href="/contact">
-                                Contact
+                        Contact Me
                     </a>
                 </Button>
                 <Button>
                     <a href="https://www.linkedin.com/in/ehsan-ashrafipour-922056245/">
-                            LinkedIn
+                        LinkedIn
                     </a>
                 </Button>
             </Box>
             <Spacer />
-            <Box>
-                <Container variant={'hero'}>
-                    <p>Ehsan Ashrafipour</p>
-                    <p>BEng Mechanical Engineering</p>
-                    <p>Full-stack Application Developer</p>
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-evenly"
+                gap={['10px', '10px', '20px', '25px']}
+            >
+                <Box>
+                    <Image
+                        borderRadius='full'
+                        boxSize={{ base: '150px', md: '200px', xl: '250px'}}
+                        src={ehsan}
+                        alt='Ehsan Ashrafipour'
+                        fallbackSrc='https://via.placeholder.com/150'
+                        loading='lazy'
+                        border='5px solid #9AA61A'
+                        boxShadow='0 0 4px 2px #3f450333'
+                    />
+                </Box>
+                <Container variant={'hero'} lineHeight={'2'} gap={['3px', '3px', '5px']}>
+                    <Box
+                        display={'flex'}
+                        flexDirection={'column'}
+                        gap={'0'}
+                        lineHeight={'1.2'}
+                    >
+                        <Text as={'h2'}>Ehsan Ashrafipour</Text>
+                        <Text
+                            as={'h3'}
+                            fontSize={['xs', 'sm', 'md', 'xl', '2xl']}
+                            fontWeight={'400'}
+                            color='brand.lightGreen'
+                        >
+                        BEng Mechanical Engineering
+                        </Text>
+                    </Box>
+                    <Text as={'h2'}>Full-stack Application Developer</Text>
                 </Container>
             </Box>
         </Flex>
