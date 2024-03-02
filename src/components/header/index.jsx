72aa344be67dacdb7c04342eeadababb982cbf8f
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-03-02 02:55:45 -0500

    perf: Refactor Home and Header components to break them to smaller components

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,7 @@
 // This file is the header component for the app.
 // It is a child component of the App component.
-// It contains the navbar component and the header text.
+// It contains the navbar component and the
+// header menu and the info container.
 // ============================================================
 
 // Importing modules and css
@@ -9,29 +10,20 @@ import {
     Flex,
     Spacer,
     Box,
-    Container,
-    Text,
     Image,
-    Tabs,
-    TabList,
-    Tab,
     useMediaQuery,
-    IconButton,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem
 } from '@chakra-ui/react';
-import { Link as ReactRouterLink, useLocation } from 'react-router-dom';
-import { Link as ChakraLink } from '@chakra-ui/react';
-import { ExternalLinkIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { useLocation } from 'react-router-dom';
 import './Header.css';
 // ============================================================
 
-// Importing images
+// Importing images and child components
 // ============================================================
 import ehsan from '../../assets/image/ehsan.png';
 import Hero from '../../assets/image/Hero.jpg';
+import Navbar from './Navbar.jsx';
+import HeaderMenu from './HeaderMenu.jsx';
+import InfoContainer from './InfoContainer.jsx';
 // ============================================================
 
 // Creating the component
@@ -49,6 +41,16 @@ const Header = () => {
     const isProjects = location.pathname === '/projects';
     const isContact = location.pathname === '/contact';
 
+    // Define the tabs to be passed to the Navbar component
+    const tabs = [
+        { name: 'Home', route: '/', isActive: isHome },
+        { name: 'Portfolio', route: '/portfolio', isActive: isPortfolio },
+        { name: 'Projects', route: '/projects', isActive: isProjects },
+        { name: 'Contact Me', route: '/contact', isActive: isContact },
+        { name: 'LinkedIn', route: 'https://www.linkedin.com/in/ehsan-ashrafipour-922056245/', isExternal: true },
+        { name: 'GitHub', route: 'https://github.com/EhsanAsh', isExternal: true }
+    ];
+
     // Define the menu items to be used in render Menu function
     const menuItems = [
         { to: '/', label: 'Home', isActive: isHome },
@@ -62,144 +64,6 @@ const Header = () => {
         },
         { to: 'https://github.com/EhsanAsh', label: 'GitHub', isExternal: true },
     ];
-
-    const renderMenu = () => (
-        <Menu>
-            <MenuButton
-                as={IconButton}
-                aria-label='Options'
-                icon={<HamburgerIcon />}
-                variant="outline"
-                _focus={{ transform: 'scale(1.2)' }}
-                _hover={{ transform: 'translateY(-5px)' }}
-                border={'2px solid'}
-                borderColor={'brand.darkBlue'}
-            />
-            <MenuList
-                bg='brand.darkBlue'
-                fontSize='sm'
-                fontWeight={'500'}
-                borderColor={'whiteAlpha.800'}
-                mr={20}
-            >
-                {menuItems.map(item => {
-                // Check if the link is external
-                    if (item.isExternal) {
-                        return (
-                            <MenuItem
-                                as="a"
-                                href={item.to}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                key={item.label}
-                                icon={item.isExternal ? <ExternalLinkIcon /> : null}
-                                _hover={{ color: 'brand.orange' }}
-                                backgroundColor={'whiteAlpha.800'}
-                            >
-                                {item.label}
-                            </MenuItem>
-                        );
-                    } else {
-                        return (
-                            <MenuItem
-                                as={ReactRouterLink}
-                                to={item.to}
-                                key={item.label}
-                                _hover={{ color: 'brand.orange' }}
-                                backgroundColor={'whiteAlpha.800'}
-                            >
-                                {item.label}
-                            </MenuItem>
-                        );
-                    }
-                })}
-            </MenuList>
-        </Menu>
-    );
-
-    // Define the tabs in render Tabs function
-    const renderTabs = () => (
-        <Tabs
-            position="relative"
-            variant="unstyled"
-            zIndex={10}
-        >
-            <TabList>
-                <Tab
-                    borderBottom={isHome ? '1px solid' : undefined}
-                    borderColor={isHome ? 'brand.orange' : undefined}
-                    transform={isHome ? 'scale(1.2)' : undefined}
-                >
-                    <ChakraLink
-                        as={ReactRouterLink}
-                        to='/'
-                        fontSize={['xs', 'xs', 'sm', 'md']}
-                    >
-                            Home
-                    </ChakraLink>
-                </Tab>
-                <Tab
-                    borderBottom={isPortfolio ? '1px solid' : undefined}
-                    borderColor={isPortfolio ? 'brand.orange' : undefined}
-                    transform={isPortfolio ? 'scale(1.2)' : undefined}
-                >
-                    <ChakraLink
-                        as={ReactRouterLink}
-                        to='/portfolio'
-                        fontSize={['xs', 'xs', 'sm', 'md']}
-                    >
-                            Portfolio
-                    </ChakraLink>
-                </Tab>
-                <Tab
-                    borderBottom={isProjects ? '1px solid' : undefined}
-                    borderColor={isProjects ? 'brand.orange' : undefined}
-                    transform={isProjects ? 'scale(1.2)' : undefined}
-                >
-                    <ChakraLink
-                        as={ReactRouterLink}
-                        to='/projects'
-                        fontSize={['xs', 'xs', 'sm', 'md']}
-                    >
-                            Projects
-                    </ChakraLink>
-                </Tab>
-                <Tab
-                    borderBottom={isContact ? '1px solid' : undefined}
-                    borderColor={isContact ? 'brand.orange' : undefined}
-                    transform={isContact ? 'scale(1.2)' : undefined}
-                >
-                    <ChakraLink
-                        as={ReactRouterLink}
-                        to='/contact'
-                        fontSize={['xs', 'xs', 'sm', 'md']}
-                    >
-                            Contact Me
-                    </ChakraLink>
-                </Tab>
-                <Tab>
-                    <ChakraLink
-                        as={ReactRouterLink}
-                        to='https://www.linkedin.com/in/ehsan-ashrafipour-922056245/'
-                        isExternal
-                        fontSize={['xs', 'xs', 'sm', 'md']}
-                    >
-                            LinkedIn<ExternalLinkIcon mx='2px' />
-                    </ChakraLink>
-                </Tab>
-                <Tab>
-                    <ChakraLink
-                        as={ReactRouterLink}
-                        to='https://github.com/EhsanAsh'
-                        isExternal
-                        fontSize={['xs', 'xs', 'sm', 'md']}
-                    >
-                            GitHub<ExternalLinkIcon mx='2px' />
-                    </ChakraLink>
-                </Tab>
-            </TabList>
-        </Tabs>
-    );
 
     return (
 
@@ -215,9 +79,14 @@ const Header = () => {
             bgImage={Hero}
             bgPosition='center'
         >
+
             {/* Render Menu or Tabs based on screen size */}
-            {isLargerThan685 ? renderTabs() : renderMenu()}
+            {isLargerThan685 ?
+                <Navbar tabs={tabs}/> :
+                <HeaderMenu menuItems={menuItems} />
+            }
             {/* Render Menu or Tabs based on screen size */}
+
             <Spacer />
             <Box
                 display="flex"
@@ -242,35 +111,9 @@ const Header = () => {
                         objectPosition='center'
                     />
                 </Box>
-                <Container variant={'hero'} lineHeight={'2'} gap={0}>
-                    <Box
-                        display={'flex'}
-                        flexDirection={'column'}
-                        alignItems={{ base: 'center', md: 'flex-start' }}
-                        gap={'0'}
-                        lineHeight={'1.2'}
-                    >
-                        <Text as={'h2'}>Ehsan Ashrafipour</Text>
-                        <Text
-                            as={'h3'}
-                            fontSize={['lg', 'xl', 'xl', 'xl', '2xl']}
-                            fontWeight={'500'}
-                        >
-                            BEng Mechanical Engineering
-                        </Text>
-                    </Box>
-                    <div className='wrapper'>
-                        <svg>
-                            <text
-                                x="50%"
-                                y="45%"
-                                textAnchor="middle"
-                            >
-                                Full-stack Application Developer
-                            </text>
-                        </svg>
-                    </div>
-                </Container>
+
+                <InfoContainer />
+
             </Box>
         </Flex>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,26 @@
-/* eslint-disable no-unused-vars */
 // Desc: This file contains the Home page component for the app.
+// Used followings as a reference:
+// https://gsap.com/resources/getting-started/Staggers
+// https://gsap.com/resources/getting-started/Easing
 // ============================================================
 
 // Import packages
 // ============================================================
-import React, { useState, useEffect, useRef } from 'react';
-import {
-    Box,
-    Button,
-    Card,
-    CardBody,
-    CardFooter,
-    CardHeader,
-    Heading,
-    Center,
-    Flex,
-    Text,
-    List,
-    ListItem,
-    ListIcon,
-} from '@chakra-ui/react';
-import {
-    InfoOutlineIcon,
-    DragHandleIcon,
-    TimeIcon,
-    StarIcon,
-    SettingsIcon,
-    CheckIcon,
-    ExternalLinkIcon
-} from '@chakra-ui/icons';
-import { Link as ReactRouterLink } from 'react-router-dom';
-import { Link as ChakraLink } from '@chakra-ui/react';
+import { useState, useEffect, useRef } from 'react';
+import { Box, Center, Flex } from '@chakra-ui/react';
 import { gsap } from 'gsap';
+// ============================================================
+
+// Import Components and Utils
+// ============================================================
+import HomeInfoCard from '../components/homeInfoCard';
+import HomeEduCard from '../components/homeEduCard';
+import { cardData, educationData } from '../utils/homeData';
 // ============================================================
 
 // Define Home function
 // ============================================================
-function Home() {
+const Home = () => {
 
     // Define state variables for the main cards
     const [currentCard, setCurrentCard] = useState(0);
@@ -116,96 +100,9 @@ function Home() {
                 width={[400, 450, 700, 850, 950]}
             >
                 <Center>
-                    {currentCard === 0 && (
-                        <Card>
-                            <CardHeader>
-                                <Heading
-                                    as="h2"
-                                    fontSize={{ base: 'md', lg: 'xl', xl: '2xl' }}
-                                    fontFamily={'josefinSlabTitle'}
-                                    color='brand.darkBlue'
-                                >
-                                WELCOME TO MY REACT PORTFOLIO!
-                                </Heading>
-                            </CardHeader>
-                            <CardBody py={3}>
-                                <Text>This single-page application portfolio is designed for employers like you who are looking for candidates with experience building single-page applications. You can use this portfolio to view my work samples and assess whether I am a good fit for your open position.</Text>
-                                <Text>Click the button below to proceed to the Portfolio page.</Text>
-                            </CardBody>
-                            <CardFooter>
-                                <Button>
-                                    <ChakraLink as={ReactRouterLink} to='/portfolio'>
-                                    Portfolio
-                                    </ChakraLink>
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    )}
-                    {currentCard === 1 && (
-                        <Card>
-                            <CardHeader>
-                                <Heading
-                                    as="h2"
-                                    fontSize={{ base: 'lg', lg: 'xl', xl: '2xl' }}
-                                    fontFamily={'josefinSlabTitle'}
-                                    color='brand.darkBlue'
-                                >
-                                PROJECTS
-                                </Heading>
-                            </CardHeader>
-                            <CardBody py={3}>
-                                <Text>AdsTrees, a revolutionary web application that intertwines the power of advertising with environmental sustainability. It offers a unique platform where viewers can watch premium ads voluntarily, and in return, trees are planted, contributing to a greener planet...</Text>
-                                <Text>Click the button below to proceed to the Projects page.</Text>
-                            </CardBody>
-                            <CardFooter>
-                                <Button>
-                                    <ChakraLink as={ReactRouterLink} to='/projects'>
-                                    Projects
-                                    </ChakraLink>
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    )}
-                    {currentCard === 2 && (
-                        <Card>
-                            <CardHeader>
-                                <Heading
-                                    as="h2"
-                                    fontSize={{ base: 'lg', lg: 'xl', xl: '2xl' }}
-                                    fontFamily={'josefinSlabTitle'}
-                                    color='brand.darkBlue'
-                                >
-                                WHY HIRE ME!
-                                </Heading>
-                            </CardHeader>
-                            <CardBody py={0}>
-                                <Text my={1}>I&apos;m a Full-Stack Developer and the University of Toronto Alumnus with a Certificate in Full-stack Application Development, boasting a remarkable graduation score of 96.07 (A). With a lifelong passion for programming and software development, I bring a blend of creativity, problem-solving skills, and a commitment to creating impactful solutions. </Text>
-                                <Text>
-                                    Click on buttons below to proceed to my LinkedIn or GitHub profile.
-                                </Text>
-                            </CardBody>
-                            <CardFooter>
-                                <Button variant='socialMediaOrange' px={0}>
-                                    <ChakraLink
-                                        as={ReactRouterLink}
-                                        to='https://www.linkedin.com/in/ehsan-ashrafipour-922056245/'
-                                        isExternal
-                                    >
-                                        <i className="fa-brands fa-linkedin fa-beat fa-2xl"></i>
-                                    </ChakraLink>
-                                </Button>
-                                <Button variant='socialMediaOrange'>
-                                    <ChakraLink
-                                        as={ReactRouterLink}
-                                        to='https://github.com/EhsanAsh'
-                                        isExternal
-                                    >
-                                        <i className="fa-brands fa-github fa-beat fa-2xl"></i>
-                                    </ChakraLink>
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    )}
+                    <HomeInfoCard
+                        cardData={cardData[currentCard]}
+                    />
                 </Center>
             </Box>
             <Box
@@ -222,130 +119,12 @@ function Home() {
                 overflow="hidden"
                 py={5}
             >
-                <Card size='lg' variant={'gsap'}>
-                    <CardHeader>
-                        <Heading
-                            as="h3"
-                            fontSize= 'lg'
-                            fontFamily={'josefinSlabTitle'}
-                            transform={'translateY(-10px)'}
-                        >
-                            University of Toronto Certificate
-                        </Heading>
-                    </CardHeader>
-                    <CardBody py={2}>
-                        <List spacing={3} lineHeight='tall'>
-                            <ListItem>
-                                <ListIcon as={InfoOutlineIcon} color='brand.orange' />
-                                    Full Stack Application Development
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={DragHandleIcon} color='brand.orange' />
-                                    University of Toronto, Toronto, Ontario
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={TimeIcon} color='brand.orange' />
-                                    July 2023 - January 2024
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={StarIcon} color='brand.orange' />
-                                    96.07 / 100 (A)
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={CheckIcon} color='brand.orange' />
-                                    HTML, CSS, JavaScript, React, NodeJS, Express, MongoDB, MySQL, and more...
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={SettingsIcon} color='brand.orange' />
-                                            Final Project |  <ChakraLink
-                                    as={ReactRouterLink}
-                                    to='https://www.adstrees.com/'
-                                    isExternal
-                                    color='brand.error'
-                                >
-                                                AdsTrees<ExternalLinkIcon mx='2px' />
-                                </ChakraLink>
-                            </ListItem>
-                        </List>
-                    </CardBody>
-                </Card>
-                <Card size='lg' variant={'gsap'}>
-                    <CardHeader>
-                        <Heading
-                            as="h3"
-                            fontSize='lg'
-                            fontFamily={'josefinSlabTitle'}
-                            transform={'translateY(-10px)'}
-                        >
-                            Bachelor&apos;s Degree
-                        </Heading>
-                    </CardHeader>
-                    <CardBody py={2}>
-                        <List spacing={3} lineHeight='tall'>
-                            <ListItem>
-                                <ListIcon as={InfoOutlineIcon} color='brand.orange' />
-                                    BEng Auto Mechanical Engineering
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={DragHandleIcon} color='brand.orange' />
-                                    Azad University, Fars, Iran
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={TimeIcon} color='brand.orange' />
-                                    January 2009 - February 2011
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={StarIcon} color='brand.orange' />
-                                    18.15 / 20 (A)
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={CheckIcon} color='brand.orange' />
-                                    Graduated with Honours
-                            </ListItem>
-                        </List>
-                    </CardBody>
-                </Card>
-                <Card size='lg' variant={'gsap'}>
-                    <CardHeader>
-                        <Heading
-                            as="h3"
-                            fontSize='lg'
-                            fontFamily={'josefinSlabTitle'}
-                            transform={'translateY(-10px)'}
-                        >
-                            Associate&apos;s Degree
-                        </Heading>
-                    </CardHeader>
-                    <CardBody py={2}>
-                        <List spacing={3} lineHeight='tall'>
-                            <ListItem>
-                                <ListIcon as={InfoOutlineIcon} color='brand.orange' />
-                                    Associate Degree in Automotive Maintenance Technology
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={DragHandleIcon} color='brand.orange' />
-                                    Shiraz University, Shiraz, Iran
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={TimeIcon} color='brand.orange' />
-                                    September 2004 - February 2007
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={StarIcon} color='brand.orange' />
-                                    18 / 20 (A)
-                            </ListItem>
-                            <ListItem>
-                                <ListIcon as={CheckIcon} color='brand.orange' />
-                                    Graduated with Honours
-                            </ListItem>
-                        </List>
-                    </CardBody>
-                </Card>
+                <HomeEduCard educationData={educationData} />
             </Box>
         </Flex>
 
     );
-}
+};
 // ============================================================
 
 // Export Home
