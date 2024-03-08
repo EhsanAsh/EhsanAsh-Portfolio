c133991f845b3692e0319f47664f3c2dbd948a0b
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-03-07 21:49:36 -0500

    perf: Refactor Portfolio page and add SkillList component

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,17 @@
-// This file is for the Portfolio page, and it is a functional component. It is a simple page that displays a short description of the website and the developer.
+// This file is for the Portfolio page. It is a simple page
+//  that displays a short description of the website and the developer.
 // ============================================================
 
-// Importing React and other important libraries
+// Importing packages and components
 // ============================================================
 import { useEffect } from 'react';
 import { useState } from 'react';
+import SkillList from '../components/skillList';
 // ============================================================
-
-// Import List component which is already styled
-// =========================================================
-import ListComponent from '../components/resume/List';
-// =========================================================
 
 // Import react-icons package
 // =========================================================
-// import { FaDownload } from 'react-icons/fa';
+// import { FaDownload, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 // endIcon={<FaDownload />}
 // =========================================================
 
@@ -26,17 +23,17 @@ const Portfolio = () => {
         {
             name: 'Github',
             url: 'https://github.com/EhsanAsh',
-            icon:1,
+            icon: 1,
         },
         {
             name: 'LinkedIn',
             url: 'https://www.linkedin.com/in/ehsan-ashrafipour-922056245/',
-            icon:2 ,
+            icon: 2,
         },
         {
             name: 'Twitter',
             url: 'https://twitter.com/EhsanAsh_5123',
-            icon:3 ,
+            icon: 3,
         },
     ];
 
@@ -52,18 +49,14 @@ const Portfolio = () => {
                 <div>
                     <div>
                         <div>
-                            <a href="https://www.linkedin.com/in/ehsan-ashrafipour-922056245/" target="_blank" rel="noreferrer">
-                                {/* <StyledMedia
-                                        image={ehsan}
-                                        title="Click to view my LinkedIn profile"
-                                        alt="Author of this website"
-                                    /> */}
-                            </a>
                             <div>
-
+                                <h2>Portfolio</h2>
                                 <p>
                                     An innovative and passionate Full-Stack Developer, recently graduated from the University of Toronto with a remarkable graduation score of 96.07 (A). I bring a rich and diverse background, combining over 10 years of experience in sales and marketing in the home and kitchen appliance sector, along with a recent but intensive journey into full-stack development. Known for an indescribable enthusiasm for coding, I am a self-proclaimed hard worker, fast learner, goal-oriented, and a collaborative team member. My career is punctuated by a childhood dream to become a programmer and application developer, a dream that I have pursued with relentless dedication. I thrive in environments that require creativity and problem-solving, always eager to develop innovative solutions that positively impact people&apos;s lives and the environment. My technical proficiency spans across various languages and frameworks, with a particular comfort in MERN stack technologies and a propensity for writing logical codes over styling and designing.
                                 </p>
+                                <div>
+                                    <SkillList />
+                                </div>
                             </div>
                             <div>
                                 {socialLinks.map((link) => (
@@ -78,27 +71,11 @@ const Portfolio = () => {
                             </div>
                         </div>
                     </div>
-
-                    <div>
-                        <div>
-                            <h2>Portfolio</h2>
-                            <div href={socialLinks[0].url} target="_blank">
-                                <button>
-                                    <span>GitHub</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
                     <div>
                         <h3>Resume</h3>
-
                         <button onClick={handleClick}>
                             <span>Download Developer Resume</span>
                         </button>
-
-                        <div>
-                            <ListComponent />
-                        </div>
                     </div>
                 </div>
             </div>
