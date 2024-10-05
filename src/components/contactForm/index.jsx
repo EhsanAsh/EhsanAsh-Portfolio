c83755e53a74b85ee56131bd4ad58dd360a5e000
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-04 20:31:11 -0400

    Change the email add

--- src/components/contactForm/index.jsx
+++ src/components/contactForm/index.jsx
@@ -10,189 +10,174 @@
 // ========================================================
 import { useEffect } from 'react';
 import {
-    Box,
-    Spacer,
-    Heading,
-    Text,
-    Button,
-    Input,
-    Link as ChakraLink,
-    Card,
-    CardBody,
-    CardHeader,
+	Box,
+	Spacer,
+	Heading,
+	Text,
+	Button,
+	Input,
+	Link as ChakraLink,
+	Card,
+	CardBody,
+	CardHeader,
 } from '@chakra-ui/react';
 // ========================================================
 
 // Creating the form component
 // ========================================================
 const ContactForm = ({
-    formData,
-    handleChange,
-    sendEmail,
-    message,
-    setMessage,
+	formData,
+	handleChange,
+	sendEmail,
+	message,
+	setMessage,
 }) => {
+	useEffect(() => {
+		let timer;
+		if (message) {
+			timer = setTimeout(() => {
+				setMessage('');
+			}, 5000);
+		}
+		return () => clearTimeout(timer); // Clear the timer when the component unmounts or the message changes
+	}, [message, setMessage]);
 
-    useEffect(() => {
-        let timer;
-        if (message) {
-            timer = setTimeout(() => {
-                setMessage('');
-            }, 5000);
-        }
-        return () => clearTimeout(timer); // Clear the timer when the component unmounts or the message changes
-    }, [message, setMessage]);
+	return (
+		<>
+			<Card size={'md'} variant={'contact'}>
+				<CardHeader>
+					<Heading
+						as={'h2'}
+						size={{ base: 'md', sm: 'lg', md: 'xl' }}
+						fontFamily={'josefinSlabTitle'}
+						fontWeight={'500'}
+						textAlign={'center'}>
+						Ways to contact me
+					</Heading>
+				</CardHeader>
+				<CardBody>
+					<Box
+						display={'flex'}
+						flexDirection={'row'}
+						justifyContent={'flex-start'}
+						alignItems={'center'}
+						gap={'10px'}
+						color={'brand.darkBlue'}>
+						<i className="fa-brands fa-google fa-xl"></i>
+						<Text color={'brand.error'} fontSize={{ base: 'sm', md: 'md' }}>
+							ehsan.ashrafipour@gmail.com
+						</Text>
+					</Box>
+					<Button
+						leftIcon={<i className="fa-brands fa-linkedin fa-2xl"></i>}
+						variant={'socialMediaContact'}>
+						<ChakraLink
+							as={'a'}
+							href={'https://www.linkedin.com/in/ehsan-ashrafipour-922056245/'}
+							isExternal>
+							<Text color={'brand.error'}>LinkedIn</Text>
+						</ChakraLink>
+					</Button>
+					<Button
+						leftIcon={<i className="fa-brands fa-square-github fa-2xl"></i>}
+						variant={'socialMediaContact'}>
+						<ChakraLink
+							as={'a'}
+							href={'https://github.com/EhsanAsh'}
+							isExternal>
+							<Text color={'brand.error'}>GitHub</Text>
+						</ChakraLink>
+					</Button>
+					<Button
+						leftIcon={<i className="fa-brands fa-square-x-twitter fa-2xl"></i>}
+						variant={'socialMediaContact'}>
+						<ChakraLink
+							as={'a'}
+							href={'https://twitter.com/EhsanAsh_5123'}
+							isExternal>
+							<Text color={'brand.error'}>Twitter</Text>
+						</ChakraLink>
+					</Button>
+				</CardBody>
+			</Card>
+			<Spacer />
+			<Box
+				display={'flex'}
+				flexDirection={'column'}
+				justifyContent={'center'}
+				alignItems={'flex-start'}
+				p={'20px'}
+				width={'100%'}
+				maxWidth={'640px'}
+				minW={'350px'}>
+				<form onSubmit={sendEmail} id="contact-form">
+					<Input
+						id="user_name"
+						name="user_name"
+						label="Name"
+						type="text"
+						value={formData.user_name || ''}
+						onChange={handleChange}
+						placeholder="Enter your name"
+						required
+						autoFocus
+					/>
 
-    return (
+					<Input
+						id="user_last_name"
+						name="user_last_name"
+						label="Last Name"
+						type="text"
+						value={formData.user_last_name || ''}
+						onChange={handleChange}
+						placeholder="Enter your last name"
+						required
+					/>
 
-        <>
+					<Input
+						id="user_email"
+						name="user_email"
+						label="Email"
+						type="email"
+						value={formData.user_email || ''}
+						onChange={handleChange}
+						placeholder="Enter your email"
+						required
+					/>
 
-            <Card size={'md'} variant={'contact'}>
-                <CardHeader>
-                    <Heading
-                        as={'h2'}
-                        size={{ base: 'md',sm: 'lg',md: 'xl' }}
-                        fontFamily={'josefinSlabTitle'}
-                        fontWeight={'500'}
-                        textAlign={'center'}
-                    >
-                        Ways to contact me
-                    </Heading>
-                </CardHeader>
-                <CardBody>
-                    <Box
-                        display={'flex'}
-                        flexDirection={'row'}
-                        justifyContent={'flex-start'}
-                        alignItems={'center'}
-                        gap={'10px'}
-                        color={'brand.darkBlue'}
-                    >
-                        <i className="fa-brands fa-google fa-xl"></i>
-                        <Text
-                            color={'brand.error'}
-                            fontSize={{ base: 'sm', md: 'md' }}
-                        >
-                            ehsan.ashrafipour
-                        </Text>
-                    </Box>
-                    <Button
-                        leftIcon={<i className="fa-brands fa-linkedin fa-2xl"></i>}
-                        variant={'socialMediaContact'}
-                    >
-                        <ChakraLink
-                            as={'a'}
-                            href={'https://www.linkedin.com/in/ehsan-ashrafipour-922056245/'}
-                            isExternal
-                        >
-                            <Text color={'brand.error'}>LinkedIn</Text>
-                        </ChakraLink>
-                    </Button>
-                    <Button
-                        leftIcon={<i className="fa-brands fa-square-github fa-2xl"></i>}
-                        variant={'socialMediaContact'}
-                    >
-                        <ChakraLink
-                            as={'a'}
-                            href={'https://github.com/EhsanAsh'}
-                            isExternal
-                        >
-                            <Text color={'brand.error'}>GitHub</Text>
-                        </ChakraLink>
-                    </Button>
-                    <Button
-                        leftIcon={<i className="fa-brands fa-square-x-twitter fa-2xl"></i>}
-                        variant={'socialMediaContact'}
-                    >
-                        <ChakraLink
-                            as={'a'}
-                            href={'https://twitter.com/EhsanAsh_5123'}
-                            isExternal
-                        >
-                            <Text color={'brand.error'}>Twitter</Text>
-                        </ChakraLink>
-                    </Button>
-                </CardBody>
-            </Card>
-            <Spacer />
-            <Box
-                display={'flex'}
-                flexDirection={'column'}
-                justifyContent={'center'}
-                alignItems={'flex-start'}
-                p={'20px'}
-                width={'100%'}
-                maxWidth={'640px'}
-                minW={'350px'}
-            >
-                <form onSubmit={sendEmail} id="contact-form">
+					<Input
+						id="message"
+						name="message"
+						label="Message"
+						type="text"
+						rows={5}
+						value={formData.message || ''}
+						onChange={handleChange}
+						placeholder="Enter your message"
+						required
+					/>
 
-                    <Input
-                        id="user_name"
-                        name="user_name"
-                        label="Name"
-                        type="text"
-                        value={formData.user_name || ''}
-                        onChange={handleChange}
-                        placeholder='Enter your name'
-                        required
-                        autoFocus
-                    />
+					<Button
+						type="submit"
+						variant={'outlineDark'}
+						alignSelf={'stretch'}
+						width={'100%'}>
+						Send
+					</Button>
 
-                    <Input
-                        id="user_last_name"
-                        name="user_last_name"
-                        label="Last Name"
-                        type="text"
-                        value={formData.user_last_name || ''}
-                        onChange={handleChange}
-                        placeholder='Enter your last name'
-                        required
-                    />
-
-                    <Input
-                        id="user_email"
-                        name="user_email"
-                        label="Email"
-                        type="email"
-                        value={formData.user_email || ''}
-                        onChange={handleChange}
-                        placeholder='Enter your email'
-                        required
-                    />
-
-                    <Input
-                        id="message"
-                        name="message"
-                        label="Message"
-                        type="text"
-                        rows={5}
-                        value={formData.message || ''}
-                        onChange={handleChange}
-                        placeholder='Enter your message'
-                        required
-                    />
-
-                    <Button
-                        type="submit"
-                        variant={'outlineDark'}
-                        alignSelf={'stretch'}
-                        width={'100%'}
-                    >
-                        Send
-                    </Button>
-
-                    {message && (
-                        <Text color={'brand.error'} fontSize={'md'} py={'3'} textAlign={'center'}>{message}</Text>
-                    )}
-
-                </form>
-            </Box>
-
-        </>
-
-    );
-
+					{message && (
+						<Text
+							color={'brand.error'}
+							fontSize={'md'}
+							py={'3'}
+							textAlign={'center'}>
+							{message}
+						</Text>
+					)}
+				</form>
+			</Box>
+		</>
+	);
 };
 // ========================================================
 
